4ab4600f6f310488206f3cd177fc0688eb34e239
Roger <rpmyrg@gmail.com>
2018-04-24 22:57:46 -0400

    Added export to window

--- src/main/ts/Tracking.ts
+++ src/main/ts/Tracking.ts
@@ -4,9 +4,14 @@ export {NotificationType} from "./tracking/notifier/NotificationType";
 export {Notifier} from "./tracking/notifier/Notifier";
 export {Subscriber} from "./tracking/notifier/Subscriber";
 
+import {Subscriber} from "./tracking/notifier/Subscriber";
+
 export class Tracking {
     public getId(): string {
         return "rpm";
+    }
+    public getNewSubscriber(id: string): Subscriber {
+        return new Subscriber(id);
     }
 }
 
